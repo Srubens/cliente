b52874fb57053308bb4e7c1125adbb36bd08b6e1
srubens <rubensfilipe6@gmail.com>
2024-06-20 14:23:19 -0300

    ajuste do texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,88 +81,88 @@ export default function Home() {
     <div className="container mt-4" >
       <div className="text-center m-4" >
         <p>
-        Preencha o formulario, alinhar os dados. Qual quer duvida entrar 
+        Preencha o formulário, alinhar os dados. Qual quer dúvida entrar 
         em contato.</p>
       </div>
       <div className="d-flex justify-content-center">
         <div className="col-md-8 col-12">
       <form onSubmit={handleSubmit} >
         <div>
-        <label >Nome do responsavel:</label>
-        <input className="form-control " 
-        type="text" name="nmresponsavel"
-        value={form.nmresponsavel}
-        onChange={onChange}
-        placeholder="Ex: Maria de Lurdes"
-        />
+          <label >Nome do responsável:</label>
+          <input className="form-control " 
+          type="text" name="nmresponsavel"
+          value={form.nmresponsavel}
+          onChange={onChange}
+          placeholder="Ex: Maria de Lurdes"
+          />
         </div>
         <br />
 
         <div>
-        <label>Nome da empresa:</label>
-        <input className="form-control" 
-        type="text" name="nmempresa" 
-        value={form.nmempresa}
-        onChange={onChange}
-        placeholder="Ex: Lojas Maria"
-        />
+          <label>Nome da empresa:</label>
+          <input className="form-control" 
+          type="text" name="nmempresa" 
+          value={form.nmempresa}
+          onChange={onChange}
+          placeholder="Ex: Lojas Maria"
+          />
         </div>
         <br />
 
         <div>
-        <label>Área de seguimento:</label>
-        <input className="form-control" type="text" 
-        name="area"
-        value={form.area}
-        onChange={onChange}
-        placeholder="Ex: Moda"
-         />
+          <label>Área de seguimento:</label>
+          <input className="form-control" type="text" 
+          name="area"
+          value={form.area}
+          onChange={onChange}
+          placeholder="Ex: Moda"
+          />
         </div>
         <br />
 
         <div>
-        <label>Possui Logo:</label>
-        <input className="form-control" 
-        type="text" name="logo"
-        value={form.logo}
-        onChange={onChange}
-        placeholder="Sim ou não"
-         />
+          <label>Possui Logo:</label>
+          <input className="form-control" 
+          type="text" name="logo"
+          value={form.logo}
+          onChange={onChange}
+          placeholder="Sim ou não"
+          />
         </div>
         <br />
 
         <div>
-        <label>Serviço ou Produto?</label>
-        <select className="form-select" name="servicoproduto" id="servicoproduto"
-        value={form.servicoproduto}
-        onChange={onChange}
-        >
-          <option value=""></option>
-          { opc1.map((opcao)=>(
-            <option key={opcao} value={opcao}>{opcao}</option>
-          )) }
-        </select>
+          <label>Serviço ou Produto?</label>
+          <select className="form-select" name="servicoproduto" id="servicoproduto"
+          value={form.servicoproduto}
+          onChange={onChange}
+          >
+            <option value=""></option>
+            { opc1.map((opcao)=>(
+              <option key={opcao} value={opcao}>{opcao}</option>
+            )) }
+          </select>
         </div>
         <br />
 
         <div>
-        <label>Outro produto ou serviço</label>
-        <input className="form-control" type="text" 
-        name="outroservicoproduto"
-        value={form.outroservicoproduto}
-        onChange={onChange}
-        placeholder="Ex: Sandalias, vestidos"
-         />
+          <label>Outro produto ou serviço</label>
+          <input className="form-control" type="text" 
+          name="outroservicoproduto"
+          value={form.outroservicoproduto}
+          onChange={onChange}
+          placeholder="Ex: Sandalias, vestidos"
+          />
         </div>
         <br />
 
         <div>
-        <label>Comentarios</label>
-        <textarea className="form-control" name="comentarios" id="comentarios"
-        value={form.comentarios}
-        onChange={onChange}
-        placeholder="Algo a acrescentar"
-        ></textarea>
+          <label>Comentarios</label>
+          <textarea className="form-control" name="comentarios" id="comentarios"
+          value={form.comentarios}
+          onChange={onChange}
+          placeholder="Algo a acrescentar"
+          ></textarea>
         </div>
 
         <button className="mt-4 btn btn-success" type="submit">Enviar</button>
